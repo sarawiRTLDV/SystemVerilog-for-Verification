749017e1c8e61a373c95b533d3d6ba99f84d67bd
sarawiRTLDV <109423168+sarawiRTLDV@users.noreply.github.com>
2022-07-24 09:47:10 +0100

    Update Datatypes.sv

--- Datatypes.sv
+++ Datatypes.sv
@@ -67,3 +67,97 @@ module tb();
   end
   
 endmodule
+
+// this is an example showing the deficulty of working with reg type 
+// if we try to use a wire in procedural assignement block it will simply throw an error 
+// similary if we try to use a reg type in continous assignement 
+// to solve this confusion system verilog provide the logic type that will behave 
+// as reg type or wire type depeding on the situation
+// the logic type is pretty useful especially in case of working with interfaces
+/*
+
+`timescale 1ns/1ps
+
+module mux(
+  input a, b, sel, 
+  output y
+			);
+  logic temp;
+  
+  
+  always@(*) begin
+    if(sel == 1) temp = a;
+    else temp = b;
+  end
+  
+  assign y = temp;
+   
+endmodule
+
+*/
+
+//in the comming example we did prove that we cannot use a reg type in case of a continous assignement
+
+
+`timescale 1ns/1ps
+
+module HalfAdder(
+            input wire a, b, 
+            output wire sum, cout
+                  );
+
+    assign sum = a ^ b;
+    assign cout = a & b;
+endmodule
+
+module FullAdder_2bits(input wire A, B, input wire Cin,
+                       output wire Sout, Cout
+                       );
+//reg s_sig, c_sig1, c_sig2; // it did throw an error because the reg type is not allowed in a wire/net connection 
+
+// so for that we have to use the wire type 
+//wire s_sig, c_sig1, c_sig2;
+logic s_sig, c_sig1, c_sig2;
+
+
+
+HalfAdder h1(A, B, s_sig, c_sig1);
+HalfAdder h2(Cin, s_sig, Sout, c_sig2);
+assign Cout = c_sig1 | c_sig2;
+
+endmodule     
+
+// to solve this confusion system verilog provide the logic type that will
+// in this part I was mistaken I though I had to built a 2bits full adder based on 1bit full adder
+/*
+`timescale 1ns/1ps                                                    
+                                                                                        
+module FullAdder(                                                                       
+            input wire a, b, cin,                                                       
+            output wire sum, cout                                                       
+                  );                                                                    
+    reg a_sig, b_sig, cin_sig, cout_sig, sum_sig;                                       
+    reg axorb;                                                                          
+                                                                                        
+    assign a_sig = a;                                                                   
+    assign b_sig = b;                                                                   
+    assign cin_sig = cin;                                                               
+                                                                                        
+    assign axorb = a_sig ^ b_sig;                                                       
+    assign cout_sig = (a_sig & b_sig) | (axorb) & cin_sig;                              
+    assign sum_sig = (axorb) ^ cin;                                                     
+    assign sum = sum_sig;                                                               
+    assign cout = cout_sig;                                                             
+endmodule                                                                               
+                                                                                        
+module FullAdder_2bits(input wire [1:0]A, [1:0]B, input wire Cin,                       
+                       output wire Sout, Cout                                           
+                       );                                                               
+FullAdder h1(A[0], B[0], Cin, s_sig, c_sig1);                                           
+FullAdder h2(A[1], B[1], s_sig, Sout, c_sig2);                                          
+assign Cout = c_sig1 | c_sig2;                                                          
+                                                                                        
+endmodule                                                                               
+*/                                                                                     
+                                                                                        
+                  
