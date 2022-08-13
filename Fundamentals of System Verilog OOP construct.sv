c90bbe3f68093772f8717b4bb8009269a7d2b745
sarawiRTLDV <109423168+sarawiRTLDV@users.noreply.github.com>
2022-08-13 11:57:17 +0100

    Update Fundamentals of System Verilog OOP construct.sv

--- Fundamentals of System Verilog OOP construct.sv
+++ Fundamentals of System Verilog OOP construct.sv
@@ -310,3 +310,37 @@ module SV();
 
 endmodule
 
+//here is how to use special functions with multiple arguments 
+
+class first;
+  
+  int data1;
+  bit [7:0] data2;
+  shortint data3;
+  
+  function new(input int data1 = 0, input bit[7:0] data2 = 8'h00, input shortint data3 = 0);
+   // to access the data member of the class we need to use the key word this
+   this.data1 = data1;
+   this.data2 = data2;
+   this.data3 = data3;    
+  endfunction
+  
+  
+endclass
+ 
+ 
+module sim_tb;
+  
+  first f1;
+  
+  initial begin
+    //f1 = new(23,,35); ///if we want to follow position
+    f1 = new( .data2(4), .data3(5), .data1(23)); //if we want to follow name
+    $display("Data1 : %0d, Data2 : %0d and Data3 : %0d", f1.data1, f1.data2, f1.data3); 
+  end
+  
+  
+endmodule
+
+
+
